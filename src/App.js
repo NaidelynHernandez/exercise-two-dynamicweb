6edf62e898fa0a0c6da4ac2bed9693799661a739
Naidelyn Hernandez <naidelynhernandez@10-18-30-169.dynapool.wireless.nyu.edu>
2022-09-20 09:39:17 -0400

    new js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import './App.css';
+
+import React from "react";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+
+import "./App.css";
+import Article from "./pages/article";
+
+import List from "./pages/list";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <list /> 
+  },
+  {
+    path:"/article",
+    element: <Article /> 
+  },
+]);
 
 
 function App() {
-  
   return (
     <div className="App">
-      <header >
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to   reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <RouterProvider router={router}/>
     </div>
   );
 }
